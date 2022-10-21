0f3f12d5e580fb021ab0827b16d74a19eb05047d
juteex <114548758+dgtuser@users.noreply.github.com>
2022-10-21 18:04:27 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+const pushbar = new Pushbar({
+    blur:true,
+    overlay:true,
+  });
